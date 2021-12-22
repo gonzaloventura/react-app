400025de8d8268b7b806cbed78bf96334e9f5a5e
Gonzalo Ventura <87400677+gonzaloventura@users.noreply.github.com>
2021-12-21 23:52:15 -0300

    Se agregó bootstrap

--- src/components/Propiedades.js
+++ src/components/Propiedades.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Propiedades(props){
     return(
         <div>
             <h2>{props.porDefecto}</h2>
-            <ul>
-                <li>{props.cadena}</li>
-                <li>{props.numero}</li>
-                <li>{props.booleano ? "Verdadero":"Falso"}</li>
-                <li>Array: {props.array.join(",")}</li>
-                <li>Al cuadrado: {props.array.map(props.funcion).join(",")}</li>
-                <li>{props.objeto.nombre + " - " + props.objeto.correo}</li>
-                <li>{props.elementoReact}</li>
-                <li>{props.componenteReact}</li>
+            <ul className="list-group">
+                <li className="list-group-item">{props.cadena}</li>
+                <li className="list-group-item">{props.numero}</li>
+                <li className="list-group-item">{props.booleano ? "Verdadero":"Falso"}</li>
+                <li className="list-group-item">Array: {props.array.join(",")}</li>
+                <li className="list-group-item">Al cuadrado: {props.array.map(props.funcion).join(",")}</li>
+                <li className="list-group-item">{props.objeto.nombre + " - " + props.objeto.correo}</li>
+                <li className="list-group-item">{props.elementoReact}</li>
+                <li className="list-group-item">{props.componenteReact}</li>
             </ul>
         </div>
     );
